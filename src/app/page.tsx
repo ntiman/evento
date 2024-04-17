e710a7f23e19debb981b2e4589ced2e7ea0a1699
ntiman <ntiman1996@gmail.com>
2024-04-16 21:29:33 -0400

    Styled header, footer and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="">Find events around you</h1>
-      <p>Browse more than 10,000 events around you</p>
+    <main className="flex flex-col items-center pt-36 px-3">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">Find events around you</h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-accent">10,000 events</span>  around you</p>
 
-      <form>
-        <input placeholder="Search events in your city..." spellCheck={false}/>
+      <form className="w-full sm:w-[580px]">
+        <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10" placeholder="Search events in your city..." spellCheck={false}/>
       </form>
 
-      <section>
+      <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular: </p>
-        <div>
+        <div className="space-x-2 font-semibold">
           <Link href="events/toronto">Toronto</Link>
           <Link href="events/oakville">Oakville</Link>
         </div>
