147905f63602da6c67c542263b02007d7250f295
ntiman <ntiman1996@gmail.com>
2024-04-16 21:42:03 -0400

    Moved search form into a separate component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+import SearchForm from "@/components/search-form";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-36 px-3">
-      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">Find events around you</h1>
-      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className="font-bold italic underline text-accent">10,000 events</span>  around you</p>
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+        Find events around you
+      </h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Browse more than{" "}
+        <span className="font-bold italic underline text-accent">
+          10,000 events
+        </span>{" "}
+        around you
+      </p>
 
-      <form className="w-full sm:w-[580px]">
-        <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10" placeholder="Search events in your city..." spellCheck={false}/>
-      </form>
+      <SearchForm/>
 
       <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular: </p>
@@ -18,5 +25,5 @@ export default function Home() {
         </div>
       </section>
     </main>
-  )
+  );
 }
